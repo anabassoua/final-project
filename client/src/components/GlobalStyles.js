a2b8c59d46b93c4bfed523b418daa1552722473d
Askia <anabassoua@gmail.com>
2023-04-01 02:12:42 -0400

    Askia Nabassoua

---
+++ client/src/components/GlobalStyles.js
@@ -0,0 +1,18 @@
+import { createGlobalStyles } from "styled-components";
+
+export default createGlobalStyles`
+:root{
+    --richblack-bg: #0d1b2a;
+    --oxfordblue-bg: #1b263b;
+    --mint: #00c896;
+}
+
+html, body, div, span, h1, h2, h3, h4, img, em, ol, ul, li, form, nav, footer, header, p{
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  box-sizing: border-box;
+
+}
+`;
