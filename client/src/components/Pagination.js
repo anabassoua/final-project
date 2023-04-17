08fd42a3d14d3e54c659e5608608c85aee4fa831
Askia <anabassoua@gmail.com>
2023-04-17 02:22:55 -0400

    Askia Nabassoua

--- client/src/components/Pagination.js
+++ client/src/components/Pagination.js
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { Icon } from "react-icons-kit";
+import { angleLeft } from "react-icons-kit/fa/angleLeft";
+import { angleRight } from "react-icons-kit/fa/angleRight";
 
 const Pagination = ({ page, setPage, totalPages }) => {
   // 1- first handle the logic when clicking
@@ -25,25 +28,61 @@ const Pagination = ({ page, setPage, totalPages }) => {
     const numbers = [];
 
     for (let i = 1; i <= totalPages; i++) {
+      const isActive = i === page;
+      const style = isActive
+        ? {
+            backgroundColor: "var(--richblack-bg)",
+            color: "white",
+            transition: "0.3s ease-in-out",
+          }
+        : null;
       numbers.push(
-        <button key={i} onClick={() => handleClick(i)}>
+        <NumberButtons key={i} onClick={() => handleClick(i)} style={style}>
           {i}
-        </button>
+        </NumberButtons>
       );
     }
     return numbers;
   };
   return (
-    <div>
-      <button onClick={handlePrev} disabled={page === 1}>
-        prev
-      </button>
-      {renderNumbers()}
-      <button onClick={handleNext} disabled={page === totalPages}>
-        next
-      </button>
-    </div>
+    <Div>
+      <ArrowButtons onClick={handlePrev} disabled={page === 1}>
+        <Icon icon={angleLeft} />
+      </ArrowButtons>
+      <NumContainer>{renderNumbers()}</NumContainer>
+      <ArrowButtons onClick={handleNext} disabled={page === totalPages}>
+        <Icon icon={angleRight} />
+      </ArrowButtons>
+    </Div>
   );
 };
+
+const Div = styled.div`
+  display: flex;
+  padding-bottom: 50px;
+  padding-top: 30px;
+`;
+
+const ArrowButtons = styled.button`
+  border-radius: 50%;
+  background: none;
+  color: inherit;
+  border: none;
+  font-size: 2rem;
+  cursor: pointer;
+`;
+
+const NumContainer = styled.div`
+  padding-left: 5px;
+  padding-right: 5px;
+  display: flex;
+  gap: 10px;
+`;
+const NumberButtons = styled.button`
+  border-radius: 10%;
+  border: none;
+  padding: 13px;
+  cursor: pointer;
+`;
 
 export default Pagination;
